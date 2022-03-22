5c5ef9121055af69c97c4b9b24cdfe6a65f077cd
JacekPad <jacekpadyjasek@gmail.com>
2022-03-22 20:09:09 +0100

    Servlety contact i about.
    Strony .jsp contact i about z zawartością z ID contact i about.

---
+++ src/main/java/pl/coderslab/web/About.java
@@ -0,0 +1,21 @@
+package pl.coderslab.web;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "about", value = "/about")
+public class About extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.getServletContext().getRequestDispatcher("/about.jsp").forward(request,response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
