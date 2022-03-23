3f0225f64ad49cebadecea278c947df04bc98062
JacekPad <jacekpadyjasek@gmail.com>
2022-03-23 16:51:40 +0100

    Dashboard

--- src/main/java/pl/coderslab/web/app/Dashboard.java
+++ src/main/java/pl/coderslab/web/app/Dashboard.java
@@ -32,10 +32,11 @@ public class Dashboard extends HttpServlet {
             request.setAttribute("recipe", numberOfRecipes);
 
             List<PlanDetails> lastPlan = planDao.getLastPlan(admin);
-            request.setAttribute("plan", lastPlan);
-            request.setAttribute("planDays", getPlanDays(lastPlan));
-            request.setAttribute("planName", getPlanName(lastPlan));
-
+            if(lastPlan.size() > 0) {
+                request.setAttribute("plan", lastPlan);
+                request.setAttribute("planDays", getPlanDays(lastPlan));
+                request.setAttribute("planName", getPlanName(lastPlan));
+            }
 
             request.getServletContext().getRequestDispatcher("/dashboard.jsp").forward(request, response);
         }
