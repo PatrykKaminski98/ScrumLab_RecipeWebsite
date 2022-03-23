db53bbcdb9ae9343934bbeed7372d5541b6e6002
PatrykKaminski98 <patryk.kaminski@interia.com>
2022-03-23 13:48:50 +0100

    szczegóły przepisu

---
+++ src/main/java/pl/coderslab/web/RecipeDetails.java
@@ -0,0 +1,37 @@
+package pl.coderslab.web;
+
+import pl.coderslab.dao.RecipeDao;
+import pl.coderslab.model.Recipe;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "RecipeDetails", value = "/app/recipe/details")
+public class RecipeDetails extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try{
+            int id = Integer.parseInt(request.getParameter("id"));
+            RecipeDao recipeDao = new RecipeDao();
+            Recipe recipe = recipeDao.read(id);
+            request.setAttribute("recipe", recipe);
+            getServletContext().getRequestDispatcher("/recipe_details.jsp")
+                    .forward(request, response);
+
+
+        }catch(NumberFormatException e){
+            e.printStackTrace();
+        }
+
+
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
